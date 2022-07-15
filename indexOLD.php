408e2d37083f42f4caf397c4725aea778a0d3a3e
Aco Vidovic <acovid@gmail.com>
2022-07-15 19:03:45 +0200

    initial commit

---
+++ indexOLD.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Domovanje.com</title>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+<style>
+h1,h2,h4{
+	font-weight: normal;
+	font-size: 20px;
+	line-height: 130%;
+	style="line-height:30px;
+	font-family: 'Ubuntu', sans-serif;}
+	a{
+	color: #D3001A;
+}
+</style>
+</head>
+<body bgcolor="white" marginheight="0" marginwidth="0" leftmargin="0" rightmargin="0" bottommargin="0" topmargin="0" style="background: #f3f3f3 url('./bg_body_01.jpg') repeat center top;">
+<table height="100%" width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td align="center" valign="top">
+<br>
+<a href="http://www.domovanje.com/"><img src="http://error.domenca.com/domovanje.gif"></a>
+<table border="0" style="border-collapse:initial; border-radius: 25px; border: 2px solid #b3b3b3; background-color: rgba(0,0,0,0.1); padding:20px;"><br><br><br>
+<tr>
+<td>
+<img src="http://error.domenca.com/warning.PNG" width="120px">
+<span style="font-size: 70px; color:#d30019;" class="glyphicon glyphicon-alert"></span>
+</td>
+<td height="85" valign="middle" style="line-height:30px; font-family:'Ubuntu', sans-serif;">
+<div style="padding-left: 20px;">
+ <h2><b>Spletna stran v izdelavi. / Website is under construction.</b></h2>
+ Za urejanje va&#154ega gostovanja sledite navodilom za <a target="_blank" href="https://podpora.domovanje.com/podpora/gostovanje/prvi-koraki-nastavitve-gostovanja/">nastavitve gostovanja</a>.
+</div>
+<br>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+<tr>
+<td align="center">
+<br>
+<table cellspacing="0" cellpadding="0" style="border-collapse: collapse;" class="tableNoBorder">
+    <tbody>
+        <tr>
+            <td align="center" style="line-height:30px; font-family:'Ubuntu', sans-serif;">
+            <h2>Potrebujete pomo&#269 pri izdelavi strani?<br />            </h2>
+            <p>Z veseljem vam bomo pomagali izbrati prave storitve za va&#154e potrebe.</p>
+            <p style="font-size:20px; color:#000000;"><img width="24" height="26" border="0" align="absmiddle" style="margin-right: 6px; border-left: 0px solid black; border-right: 0px solid black; border-width: 0px; border-style: solid; border-color: black;" alt="" src="http://error.domenca.com/icon_help_phone1.thumb-24x26.gif" /> 01 77 77 888 <a style="text-decoration:none !important; color:#000000;" href="mailto:info@domovanje.com"><img width="28" height="18" border="0" align="absmiddle" style="margin-left: 20px; margin-right: 10px; border-left: 0px solid black; border-right: 0px solid black; border-width: 0px; border-style: solid; border-color: black;" alt="" src="http://error.domenca.com/icon_help_mail1.thumb-28x18.gif" />info@domovanje.com</a></p>
+            </td>
+        </tr>
+    </tbody>
+</table>
+</td>
+</tr>
+</table>
+</body>
+</html>
